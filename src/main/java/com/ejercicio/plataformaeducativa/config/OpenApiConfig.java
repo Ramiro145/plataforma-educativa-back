40dd6d7d108218eeb935ee4dc4f5aba3a7377515
Ramiro <ramirogc145@gmail.com>
2026-01-15 14:29:10 -0600

    change-open-api

--- src/main/java/com/ejercicio/plataformaeducativa/config/OpenApiConfig.java
+++ src/main/java/com/ejercicio/plataformaeducativa/config/OpenApiConfig.java
@@ -20,6 +20,7 @@ import org.springframework.context.annotation.Configuration;
         type = SecuritySchemeType.HTTP,
         scheme = "bearer",
         bearerFormat = "JWT"
+
 )
 public class OpenApiConfig {
 }
